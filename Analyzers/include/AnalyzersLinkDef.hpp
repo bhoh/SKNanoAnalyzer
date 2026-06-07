fbb777dfbc40f92dca3eaadc4d96720088c0daee
Byung Hun, Oh <bh.oh@cern.ch>
2026-06-07 22:31:23 +0900

    add AtobbMLTree

--- Analyzers/include/AnalyzersLinkDef.hpp
+++ Analyzers/include/AnalyzersLinkDef.hpp
@@ -20,4 +20,7 @@
 #pragma link C++ class ExampleRun_base+;
 #pragma link C++ class Tutorial_reco_tt+;
 #pragma link C++ class MeasureJetTaggingEff+;
+
+#pragma link C++ class AtobbMLTree+;
+
 #endif
