61151caf975b23883c9ef2f9f832e2156919c2e4
Byung Hun, Oh <bh.oh@cern.ch>
2026-06-21 23:16:12 +0900

    add new variables

--- Analyzers/src/AtobbMLTree.cc
+++ Analyzers/src/AtobbMLTree.cc
@@ -568,6 +568,15 @@ void AtobbMLTree::FillTreeBranches(
   float bbjj_mass_13 = 0.f;
   float bbjj_mass_23 = 0.f;
 
+  float bbbW_mass_012 = 0.f;
+  float bbbW_mass_013 = 0.f;
+  float bbbW_mass_023 = 0.f;
+  float bbbW_mass_123 = 0.f;
+
+  float bW_mass_013 = 0.f;
+  float bW_mass_023 = 0.f;
+  float bW_mass_123 = 0.f;
+
 
 // Calculate kinematics for the first two b-jets
   if (bjet_indices.size() >= 2) {
@@ -577,7 +586,9 @@ void AtobbMLTree::FillTreeBranches(
     bb_mass_01 = float((b0 + b1).M());
     bb_dr_01 = float(b0.DeltaR(b1));
     bblnu_mass_01 = float((b0 + b1 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
-    bbjj_mass_01 = float((b0 + b1 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+    if (non_bjet_indices.size() >= 2) {
+      bbjj_mass_01 = float((b0 + b1 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+    }
   }
 
   // Calculate kinematics involving the third b-jet
@@ -594,8 +605,19 @@ void AtobbMLTree::FillTreeBranches(
 
     bblnu_mass_02 = float((b0 + b2 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
     bblnu_mass_12 = float((b1 + b2 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
-    bbjj_mass_02 = float((b0 + b2 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
-    bbjj_mass_12 = float((b1 + b2 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+    if (non_bjet_indices.size() >= 2) {
+      bbjj_mass_02 = float((b0 + b2 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+      bbjj_mass_12 = float((b1 + b2 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+
+      float bbbW_mass_012_cand1 = float((b0 + b1 + b2 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
+      float bbbW_mass_012_cand2 = float((b0 + b1 + b2 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+      if (std::abs(bbbW_mass_012_cand1 - const_top_mass) < std::abs(bbbW_mass_012_cand2 - const_top_mass)) {
+        bbbW_mass_012 = bbbW_mass_012_cand1;
+      }
+      else {
+        bbbW_mass_012 = bbbW_mass_012_cand2;
+      }
+    }
   }
 
   // Calculate kinematics involving the fourth b-jet
@@ -617,9 +639,48 @@ void AtobbMLTree::FillTreeBranches(
     bblnu_mass_13 = float((b1 + b3 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
     bblnu_mass_23 = float((b2 + b3 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
 
-    bbjj_mass_03 = float((b0 + b3 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
-    bbjj_mass_13 = float((b1 + b3 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
-    bbjj_mass_23 = float((b2 + b3 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+    if (non_bjet_indices.size() >= 2) {
+      bbjj_mass_03 = float((b0 + b3 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+      bbjj_mass_13 = float((b1 + b3 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+      bbjj_mass_23 = float((b2 + b3 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+
+      float bbbW_mass_013_cand1 = float((b0 + b1 + b3 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
+      float bbbW_mass_013_cand2 = float((b0 + b1 + b3 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+      float bW_mass_013_cand1 = float((b2 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+      float bW_mass_013_cand2 = float((b2 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
+      if (std::abs(bbbW_mass_013_cand1 - const_top_mass) < std::abs(bbbW_mass_013_cand2 - const_top_mass)) {
+        bbbW_mass_013 = bbbW_mass_013_cand1;
+        bW_mass_013 = bW_mass_013_cand1;
+      }
+      else {
+        bbbW_mass_013 = bbbW_mass_013_cand2;
+        bW_mass_013 = bW_mass_013_cand2;
+      }
+      float bbbW_mass_023_cand1 = float((b0 + b2 + b3 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
+      float bbbW_mass_023_cand2 = float((b0 + b2 + b3 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+      float bW_mass_023_cand1 = float((b1 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+      float bW_mass_023_cand2 = float((b1 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
+      if (std::abs(bbbW_mass_023_cand1 - const_top_mass) < std::abs(bbbW_mass_023_cand2 - const_top_mass)) {
+        bbbW_mass_023 = bbbW_mass_023_cand1;
+        bW_mass_023 = bW_mass_023_cand1;
+      }
+      else {
+        bbbW_mass_023 = bbbW_mass_023_cand2;
+        bW_mass_023 = bW_mass_023_cand2;
+      }
+      float bbbW_mass_123_cand1 = float((b1 + b2 + b3 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
+      float bbbW_mass_123_cand2 = float((b1 + b2 + b3 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+      float bW_mass_123_cand1 = float((b0 + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(0))) + static_cast<TLorentzVector>(jets.at(non_bjet_indices.at(1)))).M());
+      float bW_mass_123_cand2 = float((b0 + static_cast<TLorentzVector>(lepton) + static_cast<TLorentzVector>(METv)).M());
+      if (std::abs(bbbW_mass_123_cand1 - const_top_mass) < std::abs(bbbW_mass_123_cand2 - const_top_mass)) {
+        bbbW_mass_123 = bbbW_mass_123_cand1;
+        bW_mass_123 = bW_mass_123_cand1;
+      }
+      else {
+        bbbW_mass_123 = bbbW_mass_123_cand2;
+        bW_mass_123 = bW_mass_123_cand2;
+      }
+    }
   }
 
   // Set branches for invariant masses
@@ -637,7 +698,7 @@ void AtobbMLTree::FillTreeBranches(
   SetBranch("Training_Tree", "bb_dr_03", bb_dr_03);
   SetBranch("Training_Tree", "bb_dr_13", bb_dr_13);
   SetBranch("Training_Tree", "bb_dr_23", bb_dr_23);
-  
+
   SetBranch("Training_Tree", "bblnu_mass_01", bblnu_mass_01);
   SetBranch("Training_Tree", "bblnu_mass_02", bblnu_mass_02);
   SetBranch("Training_Tree", "bblnu_mass_12", bblnu_mass_12);
@@ -651,7 +712,260 @@ void AtobbMLTree::FillTreeBranches(
   SetBranch("Training_Tree", "bbjj_mass_13", bbjj_mass_13);
   SetBranch("Training_Tree", "bbjj_mass_23", bbjj_mass_23);
 
+  SetBranch("Training_Tree", "bbbW_mass_012", bbbW_mass_012);
+  SetBranch("Training_Tree", "bbbW_mass_013", bbbW_mass_013);
+  SetBranch("Training_Tree", "bbbW_mass_023", bbbW_mass_023);
+  SetBranch("Training_Tree", "bbbW_mass_123", bbbW_mass_123);
+  SetBranch("Training_Tree", "bW_mass_013", bW_mass_013);
+  SetBranch("Training_Tree", "bW_mass_023", bW_mass_023);
+  SetBranch("Training_Tree", "bW_mass_123", bW_mass_123);
 
+  // 1. Construct all valid combinations of (b_cand_idx, w_cand_idx)
+  struct JetCombination {
+    std::vector<unsigned int> b_cands;
+    std::vector<unsigned int> w_cands;
+  };
+  std::vector<JetCombination> jet_combinations;
+
+  std::vector<unsigned int> b_indices = bjet_indices;
+  std::vector<unsigned int> non_b_indices;
+  for (unsigned int i = 0; i < std::min<unsigned int>(6, jets.size()); i++) {
+    if (std::find(b_indices.begin(), b_indices.end(), i) == b_indices.end()) {
+      non_b_indices.push_back(i);
+    }
+  }
+
+  if (b_indices.size() >= 4) {
+    JetCombination job;
+    job.b_cands = {b_indices[0], b_indices[1], b_indices[2], b_indices[3]};
+    for (unsigned int i = 0; i < std::min<unsigned int>(6, jets.size()); i++) {
+      if (std::find(job.b_cands.begin(), job.b_cands.end(), i) == job.b_cands.end()) {
+        job.w_cands.push_back(i);
+      }
+    }
+    jet_combinations.push_back(job);
+  } else if (b_indices.size() == 3) {
+    for (unsigned int extra_b : non_b_indices) {
+      JetCombination job;
+      job.b_cands = {b_indices[0], b_indices[1], b_indices[2], extra_b};
+      for (unsigned int i = 0; i < std::min<unsigned int>(6, jets.size()); i++) {
+        if (std::find(job.b_cands.begin(), job.b_cands.end(), i) == job.b_cands.end()) {
+          job.w_cands.push_back(i);
+        }
+      }
+      if (job.w_cands.size() >= 2) {
+        jet_combinations.push_back(job);
+      }
+    }
+  }
+
+  std::vector<float> vec_AWb_mass;
+  std::vector<float> vec_AW_mass;
+  std::vector<float> vec_A_mass;
+  std::vector<float> vec_W_mass0;
+  std::vector<float> vec_Wb_mass;
+  std::vector<float> vec_W_mass1;
+  std::vector<float> vec_A_dr;
+  std::vector<float> vec_AW_dr;
+  std::vector<float> vec_AWb_dr;
+
+  // New constituent index vectors
+  std::vector<int> A_cands;
+  std::vector<int> W_signal_cands;
+  std::vector<int> top_b_signal_cands;
+  std::vector<int> W_cands;
+  std::vector<int> top_b_cands;
+
+  TLorentzVector lep = static_cast<TLorentzVector>(lepton);
+  TLorentzVector met = static_cast<TLorentzVector>(METv);
+  TLorentzVector w_lep = lep + met;
+
+  for (const auto& comb : jet_combinations) {
+    const auto& b_cand_idx = comb.b_cands;
+    const auto& w_cand_idx = comb.w_cands;
+
+    TLorentzVector b_vec[4];
+    for (int idx = 0; idx < 4; ++idx) {
+      b_vec[idx] = static_cast<TLorentzVector>(jets.at(b_cand_idx[idx]));
+    }
+
+    int comb_pairs[6][2] = {
+      {0, 1}, {0, 2}, {0, 3},
+      {1, 2}, {1, 3},
+      {2, 3}
+    };
+
+    for (int p = 0; p < 6; ++p) {
+      int i = comb_pairs[p][0];
+      int j = comb_pairs[p][1];
+
+      int k = -1, l = -1;
+      for (int idx = 0; idx < 4; ++idx) {
+        if (idx != i && idx != j) {
+          if (k == -1) k = idx;
+          else l = idx;
+        }
+      }
+
+      int rem_perms[2][2] = {
+        {k, l},
+        {l, k}
+      };
+
+      for (int perm = 0; perm < 2; ++perm) {
+        int idx_top_H = rem_perms[perm][0];
+        int idx_top_other = rem_perms[perm][1];
+
+        TLorentzVector b_A0 = b_vec[i];
+        TLorentzVector b_A1 = b_vec[j];
+        TLorentzVector b_top_H = b_vec[idx_top_H];
+        TLorentzVector b_top_other = b_vec[idx_top_other];
+
+        TLorentzVector A = b_A0 + b_A1;
+        float A_mass = float(A.M());
+        float A_dr = float(b_A0.DeltaR(b_A1));
+
+        for (size_t m = 0; m < w_cand_idx.size() - 1; ++m) {
+          for (size_t n = m + 1; n < w_cand_idx.size(); ++n) {
+            TLorentzVector j0 = static_cast<TLorentzVector>(jets.at(w_cand_idx[m]));
+            TLorentzVector j1 = static_cast<TLorentzVector>(jets.at(w_cand_idx[n]));
+            TLorentzVector w_had = j0 + j1;
+
+            // Mode 1: W_signal ( leptonic ) / W ( hadronic )
+            {
+              TLorentzVector W_signal = w_lep;
+              TLorentzVector W = w_had;
+
+              TLorentzVector AW = A + W_signal;
+              TLorentzVector AWb = AW + b_top_H;
+              TLorentzVector Wb = W + b_top_other;
+
+              vec_A_mass.push_back(A_mass);
+              vec_AW_mass.push_back(float(AW.M()));
+              vec_AWb_mass.push_back(float(AWb.M()));
+              vec_W_mass0.push_back(float(W_signal.M()));
+              vec_W_mass1.push_back(float(W.M()));
+              vec_Wb_mass.push_back(float(Wb.M()));
+
+              vec_A_dr.push_back(A_dr);
+              vec_AW_dr.push_back(float(A.DeltaR(W_signal)));
+              vec_AWb_dr.push_back(float(AW.DeltaR(b_top_H)));
+
+              // Index vectors
+              A_cands.push_back(int(b_cand_idx[i]) + 2);
+              A_cands.push_back(int(b_cand_idx[j]) + 2);
+
+              W_signal_cands.push_back(0); // Lepton
+              W_signal_cands.push_back(1); // MET
+
+              top_b_signal_cands.push_back(int(b_cand_idx[idx_top_H]) + 2);
+
+              W_cands.push_back(int(w_cand_idx[m]) + 2);
+              W_cands.push_back(int(w_cand_idx[n]) + 2);
+
+              top_b_cands.push_back(int(b_cand_idx[idx_top_other]) + 2);
+            }
+
+            // Mode 2: W_signal ( hadronic ) / W ( leptonic )
+            {
+              TLorentzVector W_signal = w_had;
+              TLorentzVector W = w_lep;
+
+              TLorentzVector AW = A + W_signal;
+              TLorentzVector AWb = AW + b_top_H;
+              TLorentzVector Wb = W + b_top_other;
+
+              vec_A_mass.push_back(A_mass);
+              vec_AW_mass.push_back(float(AW.M()));
+              vec_AWb_mass.push_back(float(AWb.M()));
+              vec_W_mass0.push_back(float(W_signal.M()));
+              vec_W_mass1.push_back(float(W.M()));
+              vec_Wb_mass.push_back(float(Wb.M()));
+
+              vec_A_dr.push_back(A_dr);
+              vec_AW_dr.push_back(float(A.DeltaR(W_signal)));
+              vec_AWb_dr.push_back(float(AW.DeltaR(b_top_H)));
+
+              // Index vectors
+              A_cands.push_back(int(b_cand_idx[i]) + 2);
+              A_cands.push_back(int(b_cand_idx[j]) + 2);
+
+              W_signal_cands.push_back(int(w_cand_idx[m]) + 2);
+              W_signal_cands.push_back(int(w_cand_idx[n]) + 2);
+
+              top_b_signal_cands.push_back(int(b_cand_idx[idx_top_H]) + 2);
+
+              W_cands.push_back(0); // Lepton
+              W_cands.push_back(1); // MET
+
+              top_b_cands.push_back(int(b_cand_idx[idx_top_other]) + 2);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  // Set vector branches for combinations
+  SetBranch("Training_Tree", "vec_AWb_mass", vec_AWb_mass);
+  SetBranch("Training_Tree", "vec_AW_mass",  vec_AW_mass);
+  SetBranch("Training_Tree", "vec_A_mass",   vec_A_mass);
+  SetBranch("Training_Tree", "vec_W_mass0",  vec_W_mass0);
+  SetBranch("Training_Tree", "vec_Wb_mass",  vec_Wb_mass);
+  SetBranch("Training_Tree", "vec_W_mass1",  vec_W_mass1);
+  SetBranch("Training_Tree", "vec_A_dr",     vec_A_dr);
+  SetBranch("Training_Tree", "vec_AW_dr",    vec_AW_dr);
+  SetBranch("Training_Tree", "vec_AWb_dr",   vec_AWb_dr);
+
+  SetBranch("Training_Tree", "A_cands",            A_cands);
+  SetBranch("Training_Tree", "W_signal_cands",     W_signal_cands);
+  SetBranch("Training_Tree", "top_b_signal_cands", top_b_signal_cands);
+  SetBranch("Training_Tree", "W_cands",            W_cands);
+  SetBranch("Training_Tree", "top_b_cands",        top_b_cands);
+
+  std::vector<float> vec_pt;
+  std::vector<float> vec_eta;
+  std::vector<float> vec_cos_phi;
+  std::vector<float> vec_sin_phi;
+  std::vector<float> vec_M;
+  std::vector<bool> vec_btagged;
+
+  // Entry 0: Lepton
+  vec_pt.push_back(float(lepton.Pt()));
+  vec_eta.push_back(float(lepton.Eta()));
+  vec_cos_phi.push_back(float(std::cos(lepton.Phi())));
+  vec_sin_phi.push_back(float(std::sin(lepton.Phi())));
+  vec_M.push_back(float(lepton.M()));
+  vec_btagged.push_back(false);
+
+  // Entry 1: MET
+  vec_pt.push_back(float(METv.Pt()));
+  vec_eta.push_back(float(METv.Eta()));
+  vec_cos_phi.push_back(float(std::cos(METv.Phi())));
+  vec_sin_phi.push_back(float(std::sin(METv.Phi())));
+  vec_M.push_back(float(METv.M()));
+  vec_btagged.push_back(false);
+
+  // Remaining entries: Jets
+  for (unsigned int i = 0; i < jets.size(); i++) {
+    const auto& jet = jets.at(i);
+    vec_pt.push_back(float(jet.Pt()));
+    vec_eta.push_back(float(jet.Eta()));
+    vec_cos_phi.push_back(float(std::cos(jet.Phi())));
+    vec_sin_phi.push_back(float(std::sin(jet.Phi())));
+    vec_M.push_back(float(jet.M()));
+
+    bool is_btag = std::find(bjet_indices.begin(), bjet_indices.end(), i) != bjet_indices.end();
+    vec_btagged.push_back(is_btag);
+  }
+
+  // Set raw vector branches
+  SetBranch("Training_Tree", "vec_pt",      vec_pt);
+  SetBranch("Training_Tree", "vec_eta",     vec_eta);
+  SetBranch("Training_Tree", "vec_cos_phi", vec_cos_phi);
+  SetBranch("Training_Tree", "vec_sin_phi", vec_sin_phi);
+  SetBranch("Training_Tree", "vec_M",       vec_M);
+  SetBranch("Training_Tree", "vec_btagged", vec_btagged);
 
   SetBranch("Training_Tree", "best_chi2", best_chi2);
   SetBranch("Training_Tree", "had_W_mass", had_W_mass);
